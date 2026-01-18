23061b2e6efafb909d1250774f290c3dacfb866b
lucasrbordignon <lucas.r.bordignon@gmail.com>
2026-01-18 11:42:25 -0300

    feat: add authentication middleware to albums and users routes

--- src/modules/albums/albums.routes.ts
+++ src/modules/albums/albums.routes.ts
@@ -1,9 +1,12 @@
-import { Router } from 'express'
+
+import { Router } from 'express';
 import { AlbumsController } from './controllers/albums.controller';
+import { ensureAuthenticated } from '../auth/middlewares/auth.middleware';
 
 const albumsRoutes = Router();
 const controller = new AlbumsController();
 
+albumsRoutes.use(ensureAuthenticated);
 albumsRoutes.post('/', controller.createAlbum.bind(controller));
 albumsRoutes.get('/:id', controller.findAlbumById.bind(controller));
 albumsRoutes.put('/:id', controller.updateAlbum.bind(controller));
--- src/modules/users/users.routes.ts
+++ src/modules/users/users.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { AlbumsController } from "../albums/controllers/albums.controller";
+import { ensureAuthenticated } from "../auth/middlewares/auth.middleware";
 
 const usersRoutes = Router();
 const albumsController = new AlbumsController();
 
+usersRoutes.use(ensureAuthenticated);
 usersRoutes.get('/:userId/albums', albumsController.findAlbumsByUserId.bind(albumsController));
 
 export { usersRoutes };
